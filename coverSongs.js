2fde1b46b430b7cada0952b3065554bbbae17bee
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-18 07:37:08 -0700

    2nd draft coverSongs

---
+++ coverSongs.js
@@ -0,0 +1,15 @@
+var playRandom = document.getElementById("button"); 
+var winnerMessage = document.getElementById("winnerMessage");
+var artist1 = docuemnt.getElementById("artist1");
+var artist2 = docuemnt.getElementById("artist2");
+var artist3 = docuemnt.getElementById("artist3");
+var 
+
+
+choices of artist button 1, 2, 3
+
+
+
+
+
+// <div id="winnerMessage">WIN or LOSE Message Here! SHOULD ROTATE TO HISTORY</div>
